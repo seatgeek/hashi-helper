f1a8d2f3413389833dc9aa009a6d519f65c0214a
Christian Winther <jippignu@gmail.com>
2017-04-21 10:13:49 +0200

    merge environment and environments structs into one file

--- config/type_environment.go
+++ config/type_environment.go
@@ -1,5 +1,7 @@
 package config
 
+import "fmt"
+
 // Environment struct
 type Environment struct {
 	Name         string
@@ -15,5 +17,36 @@ func (e Environment) merge(newEnv Environment) {
 		} else {
 			e.Applications[applicationName].merge(application)
 		}
+	}
+}
+
+// Environments struct
+type Environments map[string]Environment
+
+// Get ...
+func (e Environments) Get(env string) (Environment, error) {
+	if val, ok := e[env]; ok {
+		return val, nil
+	}
+
+	return Environment{}, fmt.Errorf("Could not find environment %s", env)
+}
+
+// Contains ...
+func (e Environments) Contains(env string) bool {
+	if _, ok := e[env]; ok {
+		return ok
+	}
+	return false
+}
+
+func (e Environments) merge(newEnvs Environments) {
+	for environmentName, environment := range newEnvs {
+		if _, ok := e[environmentName]; !ok {
+			e[environmentName] = environment
+			continue
+		}
+
+		e[environmentName].merge(environment)
 	}
 }
